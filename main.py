a4c0a60b49f38174940d40de174d53a11c16658f
Ana P <anaflorez2679@gmail.com>
2026-05-11 01:17:21 -0500

    main app first version

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from tab_analisis import contenido as analisis_contenido
+from tab_calibration import contenido as calibracion_contenido
+
+st.set_page_config(
+    page_title="Sitio para analizar espectros a partir de imágenes",
+    page_icon = "assets/icon.png",
+    layout="wide"
+)
+
+# Encabezado
+st.title("Sitio para analizar espectros a partir de imágenes")
+st.markdown("---")
+st. write("Bienvenido (a)")
+
+#Pestañas
+tab1, tab2 = st.tabs(["Análisis de imágenes","Calibración de escala"])
+
+analisis_contenido(tab1)
+calibracion_contenido(tab2)
